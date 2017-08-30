65906bd3143d57c5274724282b115c56bd8a51e2
GOEERS79 <jintorre@gmail.com>
2017-08-30 15:29:48 -0400

    move around some code

--- hellopython.py
+++ hellopython.py
@@ -2,7 +2,11 @@ import random
 import sys
 import os
 
-print('Hello World from Python 3')
+
+from myModules.methods import mysum
+
+
+#print('Hello World from Python 3')
 
 #single line comment
 
@@ -15,49 +19,54 @@ Types: Numbers Strings Lists Tuples Dictionaries
 operators: + - * / % **  //
 '''
 
+#quote = '\"Single line quote\"'
+#multi_line = ''' This is a multi line quote'''
 
-print('FLOOR: 5//2 = ' , 5//2)
+#new_string = quote + multi_line
+#print("new string = ",new_string)
 
-quote = '\"Single line quote\"'
-multi_line = ''' This is
-a multi line quote'''
-
-new_string = quote + multi_line
-print("new string = ",new_string)
-
-print('%s %s %s' %('I like it',quote,multi_line))
+#print('%s %s %s' %('I like it',quote,multi_line))
 
 
-#LISTS
-grocery_list  = ['juice','cerial','bananas']
-s = grocery_list[1:3]
-print('Modified list = ',s)
-other_list = ['Get Car', 'Shopping']
+if __name__ == '__main__':
 
-list_events = [grocery_list,other_list]
-print(list_events)
-print(list_events[0][1])
-print(len(list_events))
-print(len(grocery_list))
+    print('hello python ....')
 
-#TUPLES
+    print('sum is ', mysum(1, 1))
 
-py_tuple = (1,2,3,4,'T')
-print('py tuple = ',py_tuple)
-print('py tuple [4]= ',py_tuple[4])
-py_tuple2 = (5,2,3,4,'U')
-py_tuple3 = py_tuple + py_tuple2
-print(py_tuple3)
+    print('FLOOR: 5//2 = ', 5 // 2)
 
-#conditionals: if else elif
+    # LISTS
+    grocery_list = ['juice', 'cerial', 'bananas']
+    s = grocery_list[1:3]
+    print('Modified list = ', s)
+    other_list = ['Get Car', 'Shopping']
 
-age = 16
-if age > 16 :
-    print('You can drive')
-else :
-    print('You are not 16 yet')
+    list_events = [grocery_list, other_list]
+    print(list_events)
+    print(list_events[0][1])
+    print(len(list_events))
+    print(len(grocery_list))
 
-#logical operators:  and or not
+    # TUPLES
 
-if ((age >= 1) and (age <= 18)) :
-    print('You are sonewhere between 1 and 18')
+    py_tuple = (1, 2, 3, 4, 'T')
+    print('py tuple = ', py_tuple)
+    print('py tuple [4]= ', py_tuple[4])
+    py_tuple2 = (5, 2, 3, 4, 'U')
+    py_tuple3 = py_tuple + py_tuple2
+    print(py_tuple3)
+
+    # conditionals: if else elif
+
+    age = 16
+    if age > 16:
+        print('You can drive')
+    else:
+        print('You are not 16 yet')
+
+    # logical operators:  and or not
+
+    if ((age >= 1) and (age <= 18)):
+        print('You are sonewhere between 1 and 18')
+
